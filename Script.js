7560aa879f0ebc3b3ff7d1b2053218de839ba1de
matheuswer <editichiro@Gmail.com>
2025-06-17 09:56:14 -0300

    1

---
+++ Script.js
@@ -0,0 +1,11 @@
+const questoes = [
+    {
+        questoes: "Qual é o maior animal do mundo?",
+        alternativas: [
+            { id: 1, text: "Tubarão", correct: false },
+            { id: 2, text: "Elefante Africano", correct: false },
+            { id: 3, text: "Baleia Azul", correct: true },
+            { id: 4, text: "Orca", correct: false }
+        ]
+    }
+];
