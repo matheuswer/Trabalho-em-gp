38007463e13076196a568e4f6da49a4215f767d4
Dudariis <dudasilva51792@gmail.com>
2025-06-19 12:53:53 -0300

    maria duda

---
+++ Capa.js
@@ -0,0 +1,12 @@
+// Exemplo de comportamento ao clicar no botão
+document.getElementById("btnLogica").addEventListener("click", function() {
+    console.log("Você escolheu Lógica de Programação");
+  });
+  
+  document.getElementById("btnMatematica").addEventListener("click", function() {
+    console.log("Você escolheu Matemática");
+  });
+  
+  document.getElementById("btnHistoria").addEventListener("click", function() {
+    console.log("Você escolheu História");
+  });
